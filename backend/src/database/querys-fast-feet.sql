7cd80d361c5df31bcb665066909ff0539e8205f6
jmamadeu <jmamadeu2000@gmail.com>
2020-04-07 23:06:27 +0100

    Ends of construction of Delivery guys

--- backend/src/database/querys-fast-feet.sql
+++ backend/src/database/querys-fast-feet.sql
@@ -23,4 +23,23 @@ CREATE TABLE tb_delivery_guys (
   del_created_at TEXT NULL,
   del_updated_at TEXT NULL
 )
+
+CREATE TABLE tb_deliverys (
+  ds_id INTEGER PRIMARY KEY AUTOINCREMENT,
+  ds_recipient_id INTEGER, 
+  ds_deliveryman_id TEXT NOT NULL, 
+  ds_signature_id TEXT NULL,
+  ds_product TEXT NOT NULL,
+  ds_canceled_at TEXT NULL,
+  ds_start_date TEXT NOT NULL,
+  ds_end_date TEXT NULL,
+  ds_created_at TEX NULL,
+  ds_upadated_at TEXT NULL,
+  FOREIGN KEY (ds_recipient_id)
+    REFERENCES tb_recipients(rec_id),
+  FOREIGN KEY (ds_deliveryman_id)
+    REFERENCES tb_delivery_guys(del_id)
+)
+
+
 INSERT INTO tb_users (user_name, user_email, user_password) VALUES( 'João Amadeu', 'jmamadeu@gmail.com' ,'$2a$08$nWECOs2BtLPwFST6GHTBbOuav1pIzOIGTZ4m1N2b6OUXLXgts3qpq')
